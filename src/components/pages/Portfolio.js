d0b92a6d64ec7476ba730a3c6702df03e8ba56b1
Diego Kaku <diegokaku@gmail.com>
2022-05-15 23:46:33 -0500

    add portfolio images, add resume

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,55 +1,61 @@
 import React from 'react';
+import passwordGenerator from '../../images/password-generator.png';
+import workDayScheduler from '../../images/work-day-scheduler.png';
+import weatherDashboard from '../../images/weather-dashboard.png';
+import readmeGenerator from '../../images/readme-generator.png';
+import noteTaker from '../../images/note-taker.png';
+import budgetTracker from '../../images/budget-tracker.png';
+
 
 function Portfolio() {
   return (
     <section className="my-5">
-      {/* <h1 id="about">Password Generator</h1> */}
       <div className="my-2">
         <a href='https://github.com/kakudiego/C-3-JavaScript_Basic-Password-Generator' target="_blank" rel="noreferrer noopener">
-          <img src='https://i.imgur.com/XqQZQZL.png' alt='password generator' />
           <i className='fab fa-twitter'>
-            <span style={{ marginLeft: '10px' }}>Password Generator</span>
+            <h4 style={{ marginLeft: '10px' }}>Password Generator</h4>
           </i>
+          <img src={passwordGenerator} alt='password generator' />
         </a>
       </div>
       <div className="my-2">
         <a href='https://github.com/kakudiego/C-5-Third-Party-APIs_Work-Day-Scheduler' target="_blank" rel="noreferrer noopener">
-          <img src='https://i.imgur.com/XqQZQZL.png' alt='Work Day Scheduler' />
           <i className='fab fa-twitter'>
-            <span style={{ marginLeft: '10px' }}>Work Day Scheduler</span>
+            <h4 style={{ marginLeft: '10px' }}>Work Day Scheduler</h4>
           </i>
+          <img src={workDayScheduler} alt='Work Day Scheduler' />
         </a>
       </div>
       <div className="my-2">
         <a href='https://github.com/kakudiego/C-6-Server-Side-APIs_Weather-Dashboard' target="_blank" rel="noreferrer noopener">
-          <img src='https://i.imgur.com/XqQZQZL.png' alt='Weather-Dashboard' />
           <i className='fab fa-twitter'>
-            <span style={{ marginLeft: '10px' }}>Weather-Dashboard</span>
+            <h4 style={{ marginLeft: '10px' }}>Weather-Dashboard</h4>
           </i>
+          <img src={weatherDashboard} alt='Weather-Dashboard' />
         </a>
       </div>
       <div className="my-2">
         <a href='https://github.com/kakudiego/C-9-Node.js_Professional-README-Generator' target="_blank" rel="noreferrer noopener">
-          <img src='https://i.imgur.com/XqQZQZL.png' alt='README-Generator' />
           <i className='fab fa-twitter'>
-            <span style={{ marginLeft: '10px' }}>README-Generator</span>
+            <h4 style={{ marginLeft: '10px' }}>README-Generator</h4>
           </i>
+          <img src={readmeGenerator} alt='README-Generator' />
         </a>
       </div>
       <div className="my-2">
         <a href='https://github.com/kakudiego/C-11-Express_Note-Taker' target="_blank" rel="noreferrer noopener">
-          <img src='https://i.imgur.com/XqQZQZL.png' alt='Note-Taker' />
           <i className='fab fa-twitter'>
-            <span style={{ marginLeft: '10px' }}>Note-Taker</span>
+            <h4 style={{ marginLeft: '10px' }}>Note-Taker</h4>
           </i>
+          <img src={noteTaker} alt='Note-Taker' />
         </a>
       </div>
       <div className="my-2">
         <a href='https://github.com/kakudiego/C-19-Progressive-Web-Applications-PWA_Budget-Tracker' target="_blank" rel="noreferrer noopener">
-          <img src='https://i.imgur.com/XqQZQZL.png' alt='Budget Tracker' />
           <i className='fab fa-twitter'>
-            <span style={{ marginLeft: '10px' }}>Budget Tracker</span>
+            <h4 style={{ marginLeft: '10px' }}>Budget Tracker</h4>
           </i>
+          <img src={budgetTracker} alt='Budget Tracker' />
         </a>
       </div>
     </section>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import resume from '../../images/resume.png';
 
 function Resume() {
   return (
     <section className="my-5">
-      <h1 id="about">Resume</h1>
+      <img style={{ width: '90%', alignContent: 'center' }} src={resume} alt="resume" />
       <div className="my-2">
-        <a href='www.google.com' target="_blank" rel="noreferrer noopener">
+        <a href='https://drive.google.com/file/d/1C9oJpsVt4JVQRhJkvRnTDuqcQiRPRw0h/view?usp=sharing' target="_blank" rel="noreferrer noopener">
           <i className='fab fa-twitter'>
             <span style={{ marginLeft: '10px' }}>Download Resume</span>
           </i>

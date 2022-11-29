123b36bec130c78374661f36e1d71341072ffd1f
Diego Kaku <diegokaku@gmail.com>
2022-11-29 12:48:00 -0600

    mod resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
     <section className="my-5">
       {/* <img style={{ display: 'block', width: '80%', margin: 'auto' }} src={resume} alt="resume" /> */}
       <div className="my-2">
-        <a style={{ textDecoration: 'none' }} href='https://docs.google.com/document/d/119GHHhnkFsIBJw9iAIyTpXJCyyOaVe6y-nDfl1B67es/edit?usp=sharing' target="_blank" rel="noreferrer noopener">
+        <a style={{ textDecoration: 'none' }} href='https://docs.google.com/document/d/1_93LyKELGSkdCb8UJxZX8XA91GwY4pwh/edit?usp=sharing&ouid=104375754539868446896&rtpof=true&sd=true' target="_blank" rel="noreferrer noopener">
           <i className='fab fa-twitter'>
             <h4 style={{ textAlign: '', color: 'white' }}>Download Resume</h4>
           </i>
